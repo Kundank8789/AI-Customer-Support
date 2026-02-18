75e638508b558145578c11bfa41fa1474bef8676
kundan <kundan8789@gmail.com>
2026-02-18 22:33:02 +0530

    Creating chatbot script file

---
+++ public/chatBot.js
@@ -0,0 +1,103 @@
+(function () {
+    const api_Url = "http://localhost:3000/api/chat"
+
+    const scriptTag = document.currentScript;
+    const ownerId = scriptTag.getAttribute("data-owner-id")
+
+    if (!ownerId) {
+        console.log("Owner ID is required")
+        return;
+    }
+
+    const button = document.createElement("div");
+    button.innerHTML = "💬"
+    Object.assign(button.style, {
+        position: "fixed",
+        bottom: "24px",
+        right: "24px",
+        width: "56px",
+        height: "56px",
+        borderRadius: "50%",
+        backgroundColor: "#000",
+        color: "#fff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: "pointer",
+        fontSize: "22px",
+        boxShadow: "0 15px 49px rgba(0,0,0,0.35)",
+        zIndex: "999999",
+    })
+    document.body.appendChild(button);
+
+    const box = document.createElement("div")
+    Object.assign(box.style, {
+        position: "fixed",
+        bottom: "90px",
+        right: "24px",
+        width: "320px",
+        height: "420px",
+        background: "#fff",
+        borderRadius: "14px",
+        boxShadow: "0 25px 60px rgba(0,0,0,0.25)",
+        display: "none",
+        flexDirection: "column",
+        overflow: "hidden",
+        zIndex: "999999",
+        fontFamily: "Inter, system-ui, sans-serif",
+
+    })
+    box.innerHTML = `<div style="
+    background:#000;
+    color:#fff;
+    padding:12px 14px;
+    font-size:14px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    ">
+
+    <span>Customer Support</span>
+    <span id="chat-close" style="cursor:pointer;font-size:16px">✕</span>
+    </div>
+    <div id="chat-messages" style="
+    flex:1;
+    overflow-y:auto;
+    padding:12px;
+    background:#f5f5f5;
+    display:flex;
+    flex-direction:column;
+    "></div>
+
+    <div style="
+    display:flex;
+    border-top:1px solid #e5e7eb;
+    padding:8px;
+    gap:6px;
+    ">
+    <input id="chat-input" type="text"
+     style="
+    flex:1
+    padding:8px 10px;
+    border:1px solid #d1d5db;
+    border-radius:8px;
+    font-size:13px;
+    outline:none;
+    "
+     placeholder="Type your message..."/>
+    <button id="chat-send" style="
+    padding:8px 12px;
+    background:#000;
+    color:#fff;
+    border:none;
+    border-radius:8px;
+    cursor:pointer;
+    font-size:13px;
+    ">Send</button>
+    </div>
+
+
+    `
+    document.body.appendChild(box)
+
+})()
