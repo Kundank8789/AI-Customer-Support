3d411f89f1f773cb817f37adbf936625fdd62afd
kundan <kundan8789@gmail.com>
2026-02-24 22:34:33 +0530

    fix error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,16 @@
 import DashboardClient from '@/src/components/DashboardClient'
 import { getSession } from '@/src/lib/getSession'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 async function page() {
   const session = await getSession()
-  return (
-    <>
-      <DashboardClient ownerId={session.user.id} />
-    </>
-  )
+
+  if (!session?.user?.id) {
+    redirect("/") // or /login
+  }
+
+  return <DashboardClient ownerId={session.user.id} />
 }
 
 export default page
